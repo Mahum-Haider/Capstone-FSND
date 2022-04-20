7b47d0e4d53c48f4a275905e0cd421f077b7e6dd
Mahum-Haider <still fixing>
2022-04-21 02:01:00 +0500

    Config

--- app.py
+++ app.py
@@ -1,29 +1,84 @@
 import os
-from flask import Flask
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from models import setup_db
 from flask_cors import CORS
+import stacktrace 
+
 
 def create_app(test_config=None):
-
+    # create and configure the app
     app = Flask(__name__)
     setup_db(app)
     CORS(app)
 
-    @app.route('/')
-    def get_greeting():
-        excited = os.environ['EXCITED']
-        greeting = "Hello" 
-        if excited == 'true': 
-            greeting = greeting + "!!!!! You are doing great in this Udacity project."
-        return greeting
+    # CORS Headers
+    @app.after_request
+    def after_request(response):
+        response.headers.add("Access-Control-Allow-Headers",
+                             "Content-Type, Authorization, true")
 
-    @app.route('/coolkids')
-    def be_cool():
-        return "Be cool, man, be coooool! You're almost a FSND grad!"
+        response.headers.add("Access-Control-Allow-Methods",
+                             "GET,PUT,POST,DELETE,OPTIONS")
+
+        return response
+
+
+    @app.route('/home')
+    def handler():
+        return jsonify({
+            "success": True,
+            'message': 'Working!'
+        })
+
+
+    # @app.route('/')
+    # def get_greeting():
+    #     excited = os.environ['EXCITED']
+    #     greeting = "Hello" 
+    #     if excited == 'true': 
+    #         greeting = greeting + "!!!!! You are doing great in this Udacity project."
+    #     return greeting
+
+    # Movies
+    @app.route('/movies', methods=['GET'])
+    @requires_auth('get:movies')
+    def get_movies(payload):
+        #Returns all movies
+        movies = Movie.query.order_by(Movie.id).all()
+        movies_list = []
+
+        if len(movies) == 0:
+            abort(404)
+        else:
+            for movie in movies:
+                movies_list.append(movie.format())
+        return jsonify({
+            'success': True,
+            'movies': movies_list
+        })
+
+    # Actors
+    @app.route('/actors', methods=['GET'])
+    @requires_auth('get:actors')
+    def get_actors(payload):
+        #Returns all actors
+        actors = Actor.query.order_by(Actor.id).all()
+        actors_list = []
+
+        if len(actors) == 0:
+            abort(404)
+        else:
+            for actor in actors:
+                actors_list.append(actor.format())
+        return jsonify({
+            'success': True,
+            'actors': actors_list
+        })
 
     return app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8080, debug=True)
