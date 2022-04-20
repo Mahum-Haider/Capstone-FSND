0d351d4815356160c6ac81e72baf5263e48858a2
Mahum-Haider <still fixing>
2022-04-20 08:24:12 +0500

    heroku config

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres@localhost:5432/postgres"
+# export DATABASE_URL="postgresql://postgres@localhost:5432/postgres"
+export DATABASE_URL="postgres://yhaxjlhuegbzrd:19478662743f1b63ed71cee48ed5d775557d41e777013e9525efb77623d3b58f@ec2-34-194-158-176.compute-1.amazonaws.com:5432/d9baccfph0ft35"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
