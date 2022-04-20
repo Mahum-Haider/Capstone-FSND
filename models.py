c8f66f7acf6a64cd10688104d9b97182ff06bf16
Mahum-Haider <still fixing>
2022-04-20 23:53:09 +0500

    chngs

--- models.py
+++ models.py
@@ -2,10 +2,6 @@ import os
 from sqlalchemy import Column, String, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
-from datetime import datetime
-from dotenv import load_dotenv
-
-load_dotenv()
 
 database_path = os.environ['DATABASE_URL']
 if database_path.startswith("postgres://"):
@@ -18,78 +14,30 @@ setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
 def setup_db(app, database_path=database_path):
-  if database_path.startswith("postgres://"):
-    database_path = database_path.replace("postgres://", "postgresql://", 1)
-
-  app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-  app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-  db.app = app
-  db.init_app(app)
-  #db.create_all()
-
-def create_tables():
-  db.create_all()
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
 
 
-# Movie
-class Movie(db.Model):
-  __tablename__ = 'movies'
+'''
+Person
+Have title and release year
+'''
+class Person(db.Model):  
+  __tablename__ = 'People'
 
-  id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String, nullable=False)
-  release_year = db.Column(db.DateTime, nullable=False, default=datetime.today())
+  id = Column(db.Integer, primary_key=True)
+  name = Column(String)
+  catchphrase = Column(String)
 
-  def __init__(self, title, release_year):
-    self.title = title
-    self.release_year = release_year
-
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-
-  def update(self):
-    db.session.commit()
-
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
-
-  def format(self):
-    return {
-      'id': self.id,
-      'title': self.title,
-      'release_year': self.release_year
-    }
-
-# Actors
-class Actor(db.Model):
-  __tablename__ = 'actors'
-
-  id = db.Column(db.Integer, primary_key=True)
-  name = Column(String())
-  age = db.Column(db.Integer, nullable=False)
-  gender = db.Column(db.String, nullable=False)
-
-  def __init__(self, name, age, gender):
+  def __init__(self, name, catchphrase=""):
     self.name = name
-    self.age = age
-    self.gender = gender
-
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-
-  def update(self):
-    db.session.commit()
-
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    self.catchphrase = catchphrase
 
   def format(self):
     return {
       'id': self.id,
       'name': self.name,
-      'age': self.age,
-      'gender': self.gender
-    }
+      'catchphrase': self.catchphrase}
